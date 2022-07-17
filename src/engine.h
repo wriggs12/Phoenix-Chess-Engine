47bb63f04c350ab86275504cdf1f91c35e5bcc73
wriggs12 <wriggs12@gmail.com>
2022-07-17 17:15:22 -0400

    Initial Commit

---
+++ src/engine.h
@@ -0,0 +1,20 @@
+/****************************************************************************
+ *  Author:  Winston Riggs
+ *  License: MIT license. Full text of license is in associated file LICENSE
+ *  Copyright 2022, Winston Riggs <wriggs12@gmail.com>
+ ****************************************************************************/
+#include <vector>
+#include <iostream>
+
+#ifndef ENGINE_H
+#define ENGINE_H
+
+class Engine {
+    public:
+        Engine();
+
+    private:
+
+};
+
+#endif
